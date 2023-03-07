4960f8138b275d66245923bb6d70f2be09ea1a99
Shubham <shubhamkumarsingh5901@gmail.com>
2023-03-07 23:33:58 +0530

    deploy

--- script.js
+++ script.js
@@ -1,88 +1,89 @@
+// Selecting the DOM elements
+const alarmPicker = document.querySelector('#alarm-clock')
+const btnSetAlarm = document.querySelector('.btn-set-alarm')
+const btnClearAlarm = document.querySelector('.btn-clr-alarm')
+const clock = document.querySelector('.clock')
+const alarmList = document.querySelector('.alarms-list')
 
-const audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
-const currHour = document.getElementById('curr-hour');
-const currMinute = document.getElementById('curr-minute');
-const currSeconds = document.getElementById('curr-second');
-const currConvention = document.getElementById('curr-convention');
-const alarmDate = document.getElementById('date');
+// This audio will play when alarm starts
+const audio = new Audio(
+  'https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3',
+)
+// Audio loop set to true so audio keeps on playing
+audio.loop = true
 
-const btnSubmit = document.querySelector('[type="submit"]');
-
-const hour = document.getElementById('hour');
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const convention = document.getElementById('convention');
-
-
-const INTERVAL = 1000;
-audio.play();
-
+// Program state as an object
 const state = {
-    _hour : 0,
-    _minute: 0,
-    _second: 0,
-    _convention: '',
+  alarmTime: null,
+  alarmTimeout: null,
 }
-const dt  = new Date();
-const isAlarmSet = false;
+alarmPicker.setAttribute('min', new Date())
 
+// Function that formats the displayed time
+const formatTime = time => (time < 10 ? '0' + time : time)
 
-btnSubmit.addEventListener('click', e => {
-    e.preventDefault();
+// Function to create and display the current time
+function updateClockTime() {
+  // Getting the date from Date constructor and extract hour, minute, and second
+  const dt = new Date()
+  const hour = formatTime(dt.getHours())
+  const minute = formatTime(dt.getMinutes())
+  const second = formatTime(dt.getSeconds())
 
+  clock.innerHTML = `
+      <h2>Current Time:</h2>
+      <span>${hour} Hr</span>
+      <span>${minute} Min</span>
+      <span>${second} Sec</span>
+      <span>${hour > 12 ? 'PM' : 'AM'}</span>
+  `
+}
 
-    state._hour = (+hour.value <= 24 && +hour.value >= 1) ?  +hour.value > 12 ? +hour.value - 12 :  +hour.value : 0;
-    state._minute = (+minute.value <= 60 && +minute.value >= 1) ? +minute.value : 0;
-    state._second = (+second.value <= 60 && +second.value >= 1) ? +second.value : 0;
-    state._convention = convention.value;
+// Running interval every 1000ms to update time
+setInterval(updateClockTime, 1000)
 
-    console.log(state);
+const setAlarmTime = value => (state.alarmTime = value)
+
+function setAlarm() {
+  if (state.alarmTime) {
+    const current = new Date()
+    const timeToAlarm = new Date(state.alarmTime)
+
+    if (timeToAlarm > current) {
+      const timeout = timeToAlarm.getTime() - current.getTime()
+      state.alarmTimeout = setTimeout(() => audio.play(), timeout)
+      alert('The alarm is set for ' + timeToAlarm)
+    }
+  }
+}
+
+function stopAlarm() {
+  audio.pause()
+  if (state.alarmTimeout) {
+    clearTimeout(state.alarmTimeout)
+    alert('The alarm is cleared')
+    state.alarmTime = null,
+    state.alarmTimeout = null
+  }
+}
+
+alarmPicker.addEventListener('change', function () {
+  setAlarmTime(this.value)
 })
 
+btnSetAlarm.addEventListener('click', () => {
+  setAlarm()
+  btnSetAlarm.disabled = true
+  btnClearAlarm.disabled = false
+  if (state.alarmTime) {
+    const atime = state.alarmTime.split('T').join(' - ')
+    alarmList.innerHTML = `<li>${atime}</li>`
+  }
+})
 
-function currentTime() {
-    let hours = dt.getHours();
-    let minutes = dt.getMinutes();
-    let seconds = dt.getSeconds();
-    let convention = hours >= 12 ? 'PM' : 'AM'; 
-
-    return {hours, minutes, seconds, convention}
-}
-
-function createAlarm() {
-    const  {hours, minutes, seconds, convention} = currentTime();
-    const  {_hours, _minutes, _seconds, _convention} = state;
-
-    if(_hours >= hours){
-        console.log('Alarm ringing');
-    } 
-}
-
-
-
-function clearAlarm() {
-    state._hour = 0;
-    state._minute = 0;
-    state._second = 0;
-    state._convention = '';
-    isAlarmSet  = false;
-}
-
-setInterval(() => {
-    alarmDate.setAttribute("min", dt);
-    displayCurrentTime();
-
-},INTERVAL)
-
-function displayCurrentTime() {
-    let {hours,minutes,seconds,convention} = currentTime();  
-    // convert 24hrs into 12hrs time
-    if (hours >= 12){
-        hours -= 12;
-    }
-    currHour.textContent = hours;
-    currMinute.textContent = minutes;
-    currSeconds.textContent = seconds;
-    currConvention.textContent = convention;
-}
-
+btnClearAlarm.addEventListener('click', () => {
+  stopAlarm()
+  btnSetAlarm.disabled = false
+  alarmList.innerHTML = `<li>No alarm has been set.</li>`
+  btnClearAlarm.disabled = true
+})
